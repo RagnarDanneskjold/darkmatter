a5a392825105eaf78ecebc7de087ff632b7f7fc9
thaddeus t. grugq <the.grugq@gmail.com>
2014-03-25 17:47:19 +0700

    notes on how to add WiFi beacon detection as well.

--- src/me/opsec/darkmatter/service/RadioMonitor.java
+++ src/me/opsec/darkmatter/service/RadioMonitor.java
@@ -6,7 +6,28 @@ import android.content.Intent;
 import android.telephony.ServiceState;
 
 public class RadioMonitor {
+	/*
+	 * TODO: add support for testing whether WiFi is also unavailable. 
+	 * 
+	 * http://developer.android.com/reference/android/net/wifi/WifiManager.html#getScanResults()
+	 * 
+	 * only available after: http://developer.android.com/reference/android/net/wifi/WifiManager.html#SCAN_RESULTS_AVAILABLE_ACTION
+	 * 
+	 * 
+	 * if (serviceState == OUT_OF_SERVICE) {
+	 * 	if (wifiManager.isWifiEnabled() == true) {
+	 * 		wifiManager.startScan();
+	 * 		beacons = wifiManager.getScanResults();
+	 * 		if (beacons.length() == 0) {
+	 * 			send_security_event( RADIO_LOST );
+	 * 		}
+	 * 	} else { // no wifi available
+	 * 	send_security_event( RADIO_LOST );
+	 * } 
+	 */
+	
 	private class PhoneListener implements public PhoneStateListener {
+		
 		public void onServiceStateChanged (ServiceState serviceState) {
 			if (serviceState.getState() == ServiceState.STATE_OUT_OF_SERVICE) {
 				intent = new Intent(context, DarkService.class);
